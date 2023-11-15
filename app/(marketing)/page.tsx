8d3f8ad06789c7016f65d3deef2b41f6313a3d3d
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-15 18:05:43 +0530

    feat(Marketing): Add Landing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+import { Medal } from 'lucide-react';
+
+const MarketingPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <div>
+          <Medal />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
