77a356582dc95cc756b9c89efb49923b76f5e5bb
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-15 22:33:59 +0530

    build(SEO): Added metadata to improve SEO
    In a unique way tho!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,24 @@
 import './globals.css';
 import classNames from 'classnames';
+import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
+import { siteConfig } from '@configs/site';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Trello Clone',
-  description: 'A complete clone of Trello',
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: [
+    {
+      url: '/logo.svg',
+      href: '/logo.svg',
+    },
+  ],
 };
 
 export default function RootLayout({
