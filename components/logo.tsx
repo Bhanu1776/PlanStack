9e5a398d53b9eb42d59b450f0512a0fa0d192a48
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-12-03 20:02:50 +0530

    style(Logo): Adjusted margins

--- components/logo.tsx
+++ components/logo.tsx
@@ -11,8 +11,14 @@ const headingFont = localFont({
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
-        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+      <div className="hidden items-center gap-x-1 transition hover:opacity-75 md:flex">
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          height={30}
+          width={30}
+          className="pb-1"
+        />
         <p
           className={cn('pb-1 text-lg text-neutral-700', headingFont.className)}
         >
