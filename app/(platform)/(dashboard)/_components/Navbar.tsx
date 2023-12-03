1cdd16ecffb355fa11387d84f900d57aea545f92
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-12-03 19:31:14 +0530

    feat(Navbar): Added boards creation popover to the navbar as well

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -3,36 +3,36 @@ import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 
 import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
-// import { FormPopover } from '@/components/form/form-popover';
 
 import { MobileSidebar } from './mobile-sidebar';
+import { FormPopover } from '@components/form/form-popover';
 
 export const Navbar = () => {
   return (
     <nav className="fixed top-0 z-50 flex h-14 w-full items-center border-b bg-white px-4 shadow-sm">
-      {/* <MobileSidebar /> */}
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
         </div>
-        {/* <FormPopover align="start" side="bottom" sideOffset={18}> */}
-        <Button
-          variant="primary"
-          size="sm"
-          className="hidden h-auto rounded-sm px-2  py-1.5 md:block"
-        >
-          Create
-        </Button>
-        {/* </FormPopover> */}
-        {/* <FormPopover> */}
-        <Button
-          variant="primary"
-          size="sm"
-          className="block rounded-sm md:hidden"
-        >
-          <Plus className="h-4 w-4" />
-        </Button>
-        {/* </FormPopover> */}
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button
+            variant="primary"
+            size="sm"
+            className="hidden h-auto rounded-sm px-2  py-1.5 md:block"
+          >
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button
+            variant="primary"
+            size="sm"
+            className="block rounded-sm md:hidden"
+          >
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
